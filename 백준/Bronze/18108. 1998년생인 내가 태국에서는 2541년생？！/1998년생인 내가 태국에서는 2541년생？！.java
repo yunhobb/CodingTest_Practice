f247a74ab551fe5e33cd84cda15b3494725b3522
Yunho Jung <87285536+yunhobb@users.noreply.github.com>
2024-05-16 23:05:53 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 132 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
+        int margin = 2541 - 1998;
+
+        System.out.println(input - margin);
+    }
+}
