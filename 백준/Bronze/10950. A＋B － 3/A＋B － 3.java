4b755e8dda3c3d7229d2a70530f949df14bbc426
Yunho Jung <87285536+yunhobb@users.noreply.github.com>
2024-05-17 11:25:24 +0900

    [Bronze V] Title: A+B - 3, Time: 132 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            String[] testCase = br.readLine().split(" ");
+            System.out.println(Integer.parseInt(testCase[0]) + Integer.parseInt(testCase[1]));
+        }
+    }
+}
