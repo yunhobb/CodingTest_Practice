911093be250f314d8e82bfa202675bf566f9a1bb
Yunho Jung <87285536+yunhobb@users.noreply.github.com>
2024-05-18 19:20:14 +0900

    [Bronze V] Title: A+B - 7, Time: 128 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,23 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= t; i++) {
+            String[] inputs = br.readLine().split(" ");
+            int a = Integer.parseInt(inputs[0]);
+            int b = Integer.parseInt(inputs[1]);
+            sb.append("Case #").append(i).append(": ").append(a + b).append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
