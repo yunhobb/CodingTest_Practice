4b34a4b9acd906ca5ae71fd0f059dd3c9bbcca06
Yunho Jung <87285536+yunhobb@users.noreply.github.com>
2024-05-28 18:40:35 +0900

    [Silver IV] Title: 수 찾기, Time: 608 ms, Memory: 48604 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,44 +1,51 @@
-// https://www.acmicpc.net/problem/1920
-// 수찾기
-import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
 
-  public static void main(String[] args) throws Exception {
-    Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-    int N = sc.nextInt();
-    int[] NList = new int[N];
-    for (int i = 0; i < N; i++) {
-      NList[i] = sc.nextInt();
+        int[] nArr = new int[n];
+        String[] nInputs = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            nArr[i] = Integer.parseInt(nInputs[i]);
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        int[] mArr = new int[m];
+        String[] mInputs = br.readLine().split(" ");
+        for (int j = 0; j < m; j++) {
+            mArr[j] = Integer.parseInt(mInputs[j]);
+        }
+
+        Arrays.sort(nArr);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i : mArr) {
+            sb.append(binarySearch(nArr, i)).append("\n");
+        }
+        System.out.println(sb);
     }
-    Arrays.sort(NList); 
 
-    int m = sc.nextInt();
-    int[] MList = new int[m];
-    for (int i = 0; i < m; i++) {
-      MList[i] = sc.nextInt();
-      System.out.println(binarySearch(NList, MList[i]));
+    public static int binarySearch(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == target) {
+                return 1;
+            } else if (target < arr[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return 0;
     }
-
-  }
-
-  // 이분 탐색
-  public static int binarySearch(int[] list, int num) {
-    int min = 0;
-    int mid = 0;
-    int max = list.length - 1;
-    while (min <= max) {
-      mid = (min + max) / 2;
-      if (list[mid] == num) {
-        return 1;
-      } else if (list[mid] < num) {
-        min = mid + 1;
-      } else if (list[mid] > num) {
-        max = mid - 1;
-      }
-    }
-    return 0;
-  }
-
 }
