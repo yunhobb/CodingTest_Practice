15465325df775a4827bd7d66f2240dc176f100b4
Yunho Jung <87285536+yunhobb@users.noreply.github.com>
2024-06-06 08:55:50 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 108 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,20 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(factorial(n));
+    }
+
+    public static long factorial(long num) {
+        if (num == 0 || num == 1) {
+            return 1;
+        } else {
+            return num * factorial(num - 1);
+        }
+    }
+}
