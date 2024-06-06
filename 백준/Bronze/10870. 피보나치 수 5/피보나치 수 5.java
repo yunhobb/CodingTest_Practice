1d5b67036f030584d6bb6e38711d5c4a52eefb16
Yunho Jung <87285536+yunhobb@users.noreply.github.com>
2024-06-06 09:07:20 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 124 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,23 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(fibonacci(n));
+    }
+
+    public static int fibonacci(int num) {
+        if (num == 0) {
+            return 0;
+        } else if (num == 1) {
+            return 1;
+        } else {
+            return fibonacci(num - 2) + fibonacci(num - 1);
+        }
+    }
+}
