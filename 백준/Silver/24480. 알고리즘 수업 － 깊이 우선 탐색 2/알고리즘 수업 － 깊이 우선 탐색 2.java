c879a9917927b4685a5fdd23256797572e0be3c8
Yunho Jung <87285536+yunhobb@users.noreply.github.com>
2024-06-08 21:46:18 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 1088 ms, Memory: 91340 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    private int[] visitOrder;
+    private boolean[] visited;
+    private ArrayList<ArrayList<Integer>> graph;
+    private int order = 1;
+
+    public static void main(String[] args) throws IOException {
+        new Main().solve();
+    }
+
+    public void solve() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken()); // 정점의 수
+        int m = Integer.parseInt(st.nextToken()); // 간선의 수
+        int r = Integer.parseInt(st.nextToken()); // 시작 정점
+        
+        visitOrder = new int[n + 1];
+        visited = new boolean[n + 1];
+        graph = new ArrayList<>(n + 1);
+
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        // 내림차순 정렬
+        for (int i = 1; i <= n; i++) {
+            Collections.sort(graph.get(i), Collections.reverseOrder());
+        }
+        
+        dfs(r);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            sb.append(visitOrder[i]).append('\n');
+        }
+        System.out.print(sb.toString());
+    }
+
+    private void dfs(int node) {
+        visited[node] = true;
+        visitOrder[node] = order++;
+        
+        for (int next : graph.get(node)) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+}
