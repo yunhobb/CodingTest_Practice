6c410d8948b79c00bbfa3dcb99ce8beb75421f59
Yunho Jung <87285536+yunhobb@users.noreply.github.com>
2024-06-24 17:35:44 +0900

    [Silver III] Title: N과 M (4), Time: 124 ms, Memory: 16556 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,38 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int n, m;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+
+        n = Integer.parseInt(inputs[0]);
+        m = Integer.parseInt(inputs[1]);
+
+        arr = new int[m];
+        dfs(1, 0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int start, int depth) {
+        if (depth == m) {
+            for (int val : arr) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            arr[depth] = i;
+            dfs(i, depth + 1);
+        }
+    }
+}
