fdf485413041a44d7536b29a1d28413429d066a2
Yunho Jung <87285536+yunhobb@users.noreply.github.com>
2024-07-02 23:45:25 +0900

    [Silver II] Title: 연속합, Time: 264 ms, Memory: 25544 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,29 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        String[] inputs = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(inputs[i]);
+        }
+
+        dp[0] = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+    }
+}
